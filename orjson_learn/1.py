5de17d0169c4e8245a556738eb2372778f90a598
black <1358244533@qq.com>
2022-02-16 09:54:11 +0800

    orjson

--- orjson_learn/1.py
+++ orjson_learn/1.py
@@ -17,13 +17,27 @@ __author__ = 'Black Hole'
 import datetime
 
 import numpy as np
-import orjson
+import orjson, dataclasses
 
 data = {
     "type": "job",
     "created_at": datetime.datetime(2020, 7, 1),
     "status": "ok",
     "payload": np.array([[1, 2], [3, 4]])
+}
+_ = orjson.dumps(data, option=orjson.OPT_NAIVE_UTC | orjson.OPT_SERIALIZE_NUMPY)
+print(_)
+print(orjson.loads(_))
+
+@dataclasses.dataclass
+class A:
+    def __init__(self, a, b) -> None:
+        self.a = a
+        self.b = b
+
+data = {
+    "type": "job",
+    "a": A(a=1, b="1") 
 }
 _ = orjson.dumps(data, option=orjson.OPT_NAIVE_UTC | orjson.OPT_SERIALIZE_NUMPY)
 print(_)
