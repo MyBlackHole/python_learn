f1d98f3a5ea09bac37dcb538ef80f8ae1e78e030
black <1358244533@qq.com>
2022-02-28 13:48:43 +0800

    setcookie

---
+++ fastapi_learn/setcookie.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request
+app = FastAPI()
+
+@app.get("/cookie/")
+def Login(request: Request):
+    content = {"message": "yy_cookie"}
+    response = JSONResponse(content=content)
+    response.set_cookie(key="username", value="zlkt")
+    return response
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+    # uvicorn.run(app="app:app", host="::", port=8080, reload=True)
